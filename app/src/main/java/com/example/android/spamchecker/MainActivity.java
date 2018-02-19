a146d3e0dfaa634ffcc84c4eac01584840ad1249
harshitm98 <harshitmah98@gmail.com>
2018-02-19 12:03:24 +0530

    Receives message from NotificationService.java

--- app/src/main/java/com/example/android/spamchecker/MainActivity.java
+++ app/src/main/java/com/example/android/spamchecker/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.android.spamchecker;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private String s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.notification_text);
+        LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, new IntentFilter("Msg"));
     }
+
+    public BroadcastReceiver onNotice = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String title = intent.getStringExtra("title");
+            String text = intent.getStringExtra("text");
+
+            //Context remotePackageContext = null;
+            textView.append(title + ":" + text);
+        }
+    };
 }
